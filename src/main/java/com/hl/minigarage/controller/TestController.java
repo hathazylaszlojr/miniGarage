7b29fb736ac8a7fa0c9b10a9721540e209fff0a8
hathazylaszlojr <hathazy.laszlo@yahoo.com>
2016-12-07 23:22:06 +0200

    Add test controller

---
+++ src/main/java/com/hl/minigarage/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.hl.minigarage.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String itsWorking() {
+        return "It is working";
+    }
+
+}
