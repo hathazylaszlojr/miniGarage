d4ff3aa7c8eed6988e9b0013760dae24a68dbb03
hathazylaszlojr <hathazy.laszlo@yahoo.com>
2016-12-07 22:40:11 +0200

    Add Spring brabone

---
+++ src/main/java/com/hl/minigarage/MiniGarageApplication.java
@@ -0,0 +1,12 @@
+package com.hl.minigarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniGarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniGarageApplication.class, args);
+	}
+}
